3ebbf0b97a6f88c571260a7dcf882a12023d16f8
Ignacio <shoik.xtazy@gmail.com>
2019-11-04 22:50:19 -0300

    user almost ok

--- validations/users.js
+++ validations/users.js
@@ -37,6 +37,10 @@ const save = (user) => {
     if(!email.trim()){
         errors.email.push("El campo email es requerido");
     }
+
+    if(email.trim().length < 9 || email.trim().length > 50){
+        errors.email.push("El campo email debe tener entre 10 a 50 caracteres");
+    }
     
     if(email.trim().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) == null){
         errors.email.push("Debe ser un email válido");
@@ -45,10 +49,21 @@ const save = (user) => {
     if(!verifyDotCom(email)){
         errors.email.push("El email debe ser .cl o .com");
     }
+
+    //Phonenumber validations
+    if(phone.trim().length < 10 || phone.trim().length > 30){
+        errors.phone.push("El campo teléfono debe tener entre 10 a 30 caracteres");
+    }
+
+    if(phone.trim().match(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/) == null){
+        errors.phone.push("En numero ingresado no es correcto");
+    }
+
     
     if(errors.name.length > 0 ||
         errors.username.length > 0 ||
-        errors.email.length > 0){
+        errors.email.length > 0 ||
+        errors.phone.length > 0){
         return errors;
     }else{
         return undefined;
